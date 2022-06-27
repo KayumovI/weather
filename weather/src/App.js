c2b66773f0463c67a86a5d06d650145d4195adfe
KayumovI <ilschat98@gmail.com>
2022-06-28 01:40:51 +0400

    Добваил API

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
+
+const API_key = '5eaecfcd63a942fdfe9cf01e7d3c052b'
+const lat = '54.139556'
+const lon = '49.666127'  
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric&lang=ru`
+
 function App() {
+  
+  useEffect (() => {
+  const fetchData = async () => {
+    const response = await fetch(url) 
+    if (response.ok) {
+      const json = await response.json()
+      
+    }else {
+      console.error('Что-то не так')
+    }
+  }
+  fetchData();
+
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      Kayumov
     </div>
   );
 }
